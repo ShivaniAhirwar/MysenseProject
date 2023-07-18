138aec7ad60401a5e6f3bd4ea638ae9da94a2386
pro <pro@localhost>
2023-07-18 11:20:11 +0530

    Post and pre program

---
+++ Mysense/src/test/java/ArrayAndArrayList/Program1.java
@@ -0,0 +1,15 @@
+package ArrayAndArrayList;
+
+import java.net.MulticastSocket;
+
+public class Program1
+{
+    public static void main(String[] args) {
+		
+    	int count=23;
+    	int testCOunt=12;
+    	
+    	System.out.println((count>testCOunt)?(testCOunt>count):(testCOunt));
+    	
+	}
+}
---
+++ Mysense/src/test/java/ArrayAndArrayList/TestProgram.java
@@ -0,0 +1,16 @@
+package ArrayAndArrayList;
+
+public class TestProgram
+{
+   public static void main(String[] args) {
+	
+	   int a=12;
+	   int b=3;
+	   
+	   System.out.println("a="+--a);
+	   System.out.println("a="+a--);
+	   System.out.println("b="+b++);
+	   System.out.println("a="+a);
+	   System.out.println("b="+ ++b);
+}
+}
